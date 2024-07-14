9572b8d8fb1bb6d4208d1edfc804e68e1de91e5a
Richard Walker <richard.walker.90@gmail.com>
2024-07-14 12:33:50 -0500

    Application works. Tests needed

--- src/shout_subgroup/list_subgroup.py
+++ src/shout_subgroup/list_subgroup.py
@@ -8,8 +8,9 @@ from telegram.ext import ContextTypes
 from shout_subgroup.database import session
 from shout_subgroup.exceptions import NotGroupChatError, SubGroupDoesNotExistsError
 from shout_subgroup.models import SubgroupModel, UserModel
-from shout_subgroup.repository import find_all_subgroups_in_group_chat, \
-    find_subgroup_by_telegram_group_chat_id_and_subgroup_name, find_all_users_in_subgroup
+from shout_subgroup.repository import (find_all_subgroups_in_group_chat,
+                                       find_subgroup_by_telegram_group_chat_id_and_subgroup_name,
+                                       find_all_users_in_subgroup)
 from shout_subgroup.utils import is_group_chat
 
 
@@ -36,7 +37,7 @@ async def _handle_list_subgroups(update: Update, db: Session, telegram_group_cha
         return
 
     # "mock-subgroup-1, mock-subgroup-2, mock-subgroup-3"
-    subgroups_names = [sub.name for sub in subgroups]
+    subgroups_names = [f"'{sub.name}'" for sub in subgroups]
     joined_subgroup_names = ", ".join(subgroups_names)
 
     await update.message.reply_text(f"Here are the subgroups for this chat: {joined_subgroup_names}")
@@ -56,12 +57,21 @@ async def list_subgroups(db: Session, telegram_group_chat_id: int) -> list[Type[
 
 
 async def _handle_list_subgroup_members(update: Update, db: Session, telegram_group_chat_id: int, subgroup_name: str):
-    await update.message.reply_text(f"MOCK SUBGROUP RESPONSE. Subgroup name = {subgroup_name}")
-    pass
+    members = await list_subgroup_members(db, telegram_group_chat_id, subgroup_name)
+
+    if not members:
+        await update.message.reply_text(f"'{subgroup_name}' subgroup does not have any members")
+        return
+
+    # "@username1, @username2, @username3"
+    usernames = [f"@{member.username}" for member in members]
+    joined_usernames = ", ".join(usernames)
+
+    await update.message.reply_text(f"'{subgroup_name}' subgroup has these members: {joined_usernames}")
+    return
 
 
 async def list_subgroup_members(db: Session, telegram_group_chat_id: int, subgroup_name: str) -> list[Type[UserModel]]:
-
     # Guard Clauses
     if not await is_group_chat(telegram_group_chat_id):
         msg = f"Can't list subgroups because telegram chat id {telegram_group_chat_id} is not a group chat."
@@ -69,7 +79,8 @@ async def list_subgroup_members(db: Session, telegram_group_chat_id: int, subgro
         raise NotGroupChatError(msg)
 
     # If subgroup does not exist throw exception
-    subgroup = await find_subgroup_by_telegram_group_chat_id_and_subgroup_name(db, telegram_group_chat_id, subgroup_name)
+    subgroup = await find_subgroup_by_telegram_group_chat_id_and_subgroup_name(db, telegram_group_chat_id,
+                                                                               subgroup_name)
     if not subgroup:
         msg = f"Subgroup: {subgroup_name} does not exist in telegram group chat: {telegram_group_chat_id}"
         logging.info(msg)
@@ -106,5 +117,5 @@ async def list_subgroup_handler(update: Update, context: ContextTypes.DEFAULT_TY
         await update.message.reply_text("Sorry, you can only list subgroups in group chats.")
         return
     except SubGroupDoesNotExistsError:
-        await update.message.reply_text(f"Subgroup {subgroup_name} does not exist.")
+        await update.message.reply_text(f"Subgroup '{subgroup_name}' does not exist.")
         return
