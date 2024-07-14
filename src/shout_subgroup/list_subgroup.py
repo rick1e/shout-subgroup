abb2e2bf271c1165d915c72cffd40892c36d70b6
Richard Walker <richard.walker.90@gmail.com>
2024-07-14 12:18:02 -0500

    Handle SubGroupDoesNotExistsError

--- src/shout_subgroup/list_subgroup.py
+++ src/shout_subgroup/list_subgroup.py
@@ -6,9 +6,10 @@ from telegram import Update
 from telegram.ext import ContextTypes
 
 from shout_subgroup.database import session
-from shout_subgroup.exceptions import NotGroupChatError
-from shout_subgroup.models import SubgroupModel
-from shout_subgroup.repository import find_all_subgroups_in_group_chat
+from shout_subgroup.exceptions import NotGroupChatError, SubGroupDoesNotExistsError
+from shout_subgroup.models import SubgroupModel, UserModel
+from shout_subgroup.repository import find_all_subgroups_in_group_chat, \
+    find_subgroup_by_telegram_group_chat_id_and_subgroup_name, find_all_users_in_subgroup
 from shout_subgroup.utils import is_group_chat
 
 
@@ -59,6 +60,26 @@ async def _handle_list_subgroup_members(update: Update, db: Session, telegram_gr
     pass
 
 
+async def list_subgroup_members(db: Session, telegram_group_chat_id: int, subgroup_name: str) -> list[Type[UserModel]]:
+
+    # Guard Clauses
+    if not await is_group_chat(telegram_group_chat_id):
+        msg = f"Can't list subgroups because telegram chat id {telegram_group_chat_id} is not a group chat."
+        logging.info(msg)
+        raise NotGroupChatError(msg)
+
+    # If subgroup does not exist throw exception
+    subgroup = await find_subgroup_by_telegram_group_chat_id_and_subgroup_name(db, telegram_group_chat_id, subgroup_name)
+    if not subgroup:
+        msg = f"Subgroup: {subgroup_name} does not exist in telegram group chat: {telegram_group_chat_id}"
+        logging.info(msg)
+        raise SubGroupDoesNotExistsError(msg)
+
+    # Get all the users for the subgroup
+    users = await find_all_users_in_subgroup(db, subgroup.group_chat_id, subgroup_name)
+    return users
+
+
 async def list_subgroup_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """
     Handles the list subgroup command.
@@ -71,18 +92,19 @@ async def list_subgroup_handler(update: Update, context: ContextTypes.DEFAULT_TY
     """
 
     args = context.args
-
-    # If they provide the subgroup name, then we'll list the members
-    # Else: default to listing all the subgroups
     chat_id = update.effective_chat.id
+    subgroup_name = args[0] if len(args) == 1 else ""
 
     try:
-        if len(args) == 1:
-            subgroup_name = args[0]
+        # If the subgroup name doesn't exist, we'll default to listing the subgroups
+        if subgroup_name:
             await _handle_list_subgroup_members(update, session, chat_id, subgroup_name)
         else:
             await _handle_list_subgroups(update, session, chat_id)
 
     except NotGroupChatError:
         await update.message.reply_text("Sorry, you can only list subgroups in group chats.")
+        return
+    except SubGroupDoesNotExistsError:
+        await update.message.reply_text(f"Subgroup {subgroup_name} does not exist.")
         return
