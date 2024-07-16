fc4bcd6fe7f0bf04dbe08e1879d43710dd10b60d
Richard Walker <richard.walker.90@gmail.com>
2024-07-16 16:54:20 -0500

    Able to detect when subgroup already exists

--- src/shout_subgroup/main.py
+++ src/shout_subgroup/main.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from telegram.ext import ApplicationBuilder, CommandHandler
 
 from shout import shout_handler
-from subgroup import subgroup_handler
+from modify_subgroup import subgroup_handler
 from shout_subgroup.list_subgroup import list_subgroup_handler
 
 # Your bot's token
--- src/shout_subgroup/modify_subgroup.py
+++ src/shout_subgroup/modify_subgroup.py
@@ -18,7 +18,6 @@ async def create_subgroup(db: Session,
                           telegram_chat: Chat,
                           subgroup_name: str,
                           usernames: set[str]) -> SubgroupModel:
-
     telegram_chat_id = telegram_chat.id
     if not await is_group_chat(telegram_chat_id):
         msg = f"Can't create subgroup because telegram chat id {telegram_chat_id} is not a group chat."
@@ -51,6 +50,23 @@ async def create_subgroup(db: Session,
     return created_subgroup
 
 
+async def does_subgroup_exist(db: Session, telegram_group_chat_id: int, subgroup_name: str) -> bool:
+
+    # TODO: Add test
+    if not await is_group_chat(telegram_group_chat_id):
+        msg = f"Can't list subgroups because telegram chat id {telegram_group_chat_id} is not a group chat."
+        logging.info(msg)
+        raise NotGroupChatError(msg)
+
+    subgroup = await find_subgroup_by_telegram_group_chat_id_and_subgroup_name(
+        db,
+        telegram_group_chat_id,
+        subgroup_name
+    )
+
+    return subgroup is not None
+
+
 async def subgroup_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """
     Handles the create subgroup command.
@@ -70,21 +86,28 @@ async def subgroup_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -
         await update.message.reply_text(msg)
         return
 
+    chat_id = update.effective_chat.id
+    subgroup_name = args[0]
+
     usernames = {name.replace("@", "") for name in set(args[1:])}  # removing mention from args for usernames
     if not await usernames_valid(usernames):
         await update.message.reply_text("Not all the usernames are valid. Please re-check what you entered.")
 
-    subgroup_name = args[0]
-
     try:
 
-        subgroup = await create_subgroup(session, update.effective_chat, subgroup_name, usernames)
-        subgroup_usernames = [user.username for user in subgroup.users]
-        await update.message.reply_text(f"Subgroup {subgroup.name} was created with users {subgroup_usernames}")
-        return
+        is_existing_subgroup = await does_subgroup_exist(session, chat_id, subgroup_name)
+        if is_existing_subgroup:
+            # TODO: implement service logic
+            await update.message.reply_text(f"Mock reply when subgroup {subgroup_name} already exists")
+            return
+        else:
+            subgroup = await create_subgroup(session, update.effective_chat, subgroup_name, usernames)
+            subgroup_usernames = [user.username for user in subgroup.users]
+            await update.message.reply_text(f"Subgroup {subgroup.name} was created with users {subgroup_usernames}")
+            return
 
     except NotGroupChatError:
-        await update.message.reply_text("Sorry, you can only create subgroups in group chats.")
+        await update.message.reply_text("Sorry, you can only create or modify subgroups in group chats.")
         return
 
     except SubGroupExistsError:
