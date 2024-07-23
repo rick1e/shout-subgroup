b62b1024bbac9616d97ba9077dadb5fa7dba04a0
Richard Walker <richard.walker.90@gmail.com>
2024-07-22 21:51:17 -0500

    First test passes

--- tests/test_group_chat_listener.py
+++ tests/test_group_chat_listener.py
@@ -1,7 +1,12 @@
+from unittest.mock import Mock
+
 import pytest
 from sqlalchemy.orm import Session
+from telegram import Chat
 
 from shout_subgroup.exceptions import SubGroupDoesNotExistsError, UserDoesNotExistsError, NotGroupChatError
+from shout_subgroup.group_chat_listener import add_user_to_group_chat
+from shout_subgroup.models import UserModel
 from shout_subgroup.modify_subgroup import add_users_to_existing_subgroup, remove_users_from_existing_subgroup
 from test_helpers import create_test_user, create_test_group_chat, create_test_subgroup
 
@@ -16,10 +21,41 @@ class MockTelegramChat:
 @pytest.mark.asyncio
 async def test_add_user_if_not_in_group_chat(db: Session):
     # Given: A group chat exists
+    john = create_test_user(db, telegram_user_id=12345, username="johndoe", first_name="John", last_name="Doe")
+    jane = create_test_user(db, telegram_user_id=67890, username="janedoe", first_name="Jane", last_name="Doe")
+
+    telegram_group_chat_id = -123456789
+    telegram_group_chat_name = "Group Chat"
+    telegram_group_chat_description = "Test Chatting"
+
+    initial_group_chat_users = [john, jane]
+    group_chat = create_test_group_chat(db, telegram_group_chat_id, telegram_group_chat_name, initial_group_chat_users)
+
     # But: The user is not in the group chat
+    betty = UserModel(
+        telegram_user_id=87654,
+        username="betty",
+        first_name="Betty",
+        last_name="White"
+    )
+
+    telegram_chat = Mock()
+    telegram_chat.id = telegram_group_chat_id
+    telegram_chat.name = telegram_group_chat_name
+    telegram_chat.description = telegram_group_chat_description
+
     # When: The listener determines this
+    added_user = await add_user_to_group_chat(db, telegram_chat, betty)
+
     # Then: The user should be added to the group chat
-    pass
+    assert len(group_chat.users) == 3
+
+    assert added_user.user_id is not None
+
+    actual_user = next((user for user in group_chat.users if user.username == betty.username), None)
+    assert actual_user.first_name == added_user.first_name
+    assert actual_user.last_name == added_user.last_name
+    assert actual_user.telegram_user_id == added_user.telegram_user_id
 
 
 @pytest.mark.asyncio
