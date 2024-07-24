155294bdd5b905fe9d633ba8790a51c78ba9520f
rick1e <alrickab@gmail.com>
2024-07-24 14:53:39 -0500

    made update for PR

--- src/shout_subgroup/database.py
+++ src/shout_subgroup/database.py
@@ -14,8 +14,9 @@ session = Session()
 
 # Create users
 richie = UserModel(telegram_user_id=12345, username="ashcir", first_name="Richie", last_name="Doe")
-# alrick = UserModel(telegram_user_id=67890, username="alrickb", first_name="Alrick", last_name="Rain")
-alrick = UserModel(telegram_user_id=67890, username="alrick", first_name="Alrick", last_name="Rain")
+alrick = UserModel(telegram_user_id=67890, username="alrickb", first_name="Alrick", last_name="Rain")
+# Going leave it there in case i need it for testing
+# alrick = UserModel(telegram_user_id=67890, username="alrick", first_name="Alrick", last_name="Rain")
 
 session.add(richie)
 session.add(alrick)
