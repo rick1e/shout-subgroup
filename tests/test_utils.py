286757ae094d23cd4aa7afd3183e67ea19cfd213
Richard Walker <richard.walker.90@gmail.com>
2024-08-04 20:37:42 -0400

    Users can mention themselves when creating and modifying groups (#41)
    fixes #16

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,8 @@
+from unittest.mock import Mock
+
 import pytest
 
-from shout_subgroup.utils import is_group_chat
+from shout_subgroup.utils import is_group_chat, format_telegram_usernames
 
 
 @pytest.mark.asyncio
@@ -11,6 +13,28 @@ from shout_subgroup.utils import is_group_chat
 async def test_is_group_chat(telegram_chat_id, expected_result):
     # When: We check if it's a group chat
     result = await is_group_chat(telegram_chat_id)
+
+    # Then: The correct value is returned
+    assert result == expected_result
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("usernames, expected_result", [
+    ({"@me", "@pablo", "@garcia"}, {"richie", "pablo", "garcia"}),  # @me is present
+    ({"@ME", "@pablo", "@garcia"}, {"richie", "pablo", "garcia"}),  # Uppercase
+    ({"@mE", "@pablo", "@garcia"}, {"richie", "pablo", "garcia"}),  # Mixed case
+    ({"@richie", "@pablo", "@garcia"}, {"richie", "pablo", "garcia"}),  # No @me
+    ({}, set()),  # Empty set
+])
+async def test_format_telegram_usernames(usernames, expected_result):
+    # Given: We have the telegram user
+    telegram_user = Mock()
+    telegram_user.id = 123
+    telegram_user.first_name = "Richard"
+    telegram_user.username = "richie"
+
+    # When: We format the usernames
+    result = await format_telegram_usernames(usernames, telegram_user)
 
     # Then: The correct value is returned
     assert result == expected_result
