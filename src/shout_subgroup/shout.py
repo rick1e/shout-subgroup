86a4460680372e2292f1b3e3c1bd6413584da3e9
Richard Walker <richard.walker.90@gmail.com>
2024-09-23 20:39:31 -0400

    Fix /shout throws an error if no users are in the group chat (#67)

--- src/shout_subgroup/shout.py
+++ src/shout_subgroup/shout.py
@@ -13,27 +13,39 @@ from shout_subgroup.repository import find_all_users_in_group_chat, find_all_use
     find_group_chat_by_telegram_group_chat_id
 from shout_subgroup.utils import is_group_chat, create_mention_from_user
 
+logger = logging.getLogger(__name__)
+
 
 async def shout_subgroup_members(db: Session, telegram_chat_id: int, subgroup_name: str) -> str:
     if not await is_group_chat(telegram_chat_id):
-        msg = f"Can't create subgroup because telegram chat id {telegram_chat_id} is not a group chat."
-        logging.info(msg)
+        msg = f"Can't shout subgroup members because telegram chat id {telegram_chat_id} is not a group chat."
+        logger.info(msg)
         raise NotGroupChatError(msg)
 
     group_chat = await find_group_chat_by_telegram_group_chat_id(db, telegram_chat_id)
     if not group_chat:
         msg = f"Group chat with telegram_chat_id: {telegram_chat_id} was not found. "
-        logging.info(msg)
+        logger.info(msg)
         raise GroupChatDoesNotExistError(msg)
 
-    all_members = await find_all_users_in_subgroup(db, group_chat.group_chat_id, subgroup_name)
-    message = create_message_to_mention_members(all_members)
+    subgroup_members = await find_all_users_in_subgroup(db, group_chat.group_chat_id, subgroup_name)
+
+    if not subgroup_members:
+        logger.info(f"Attempted to shout subgroup '{subgroup_name}' in telegram chat id: {telegram_chat_id} members, but there are no members.")
+        return f"'{subgroup_name}' subgroup has no members, use /group to add members."
+
+    message = create_message_to_mention_members(subgroup_members)
     return message
 
 
 async def shout_all_members(db: Session, telegram_group_chat_id: int) -> str:
-    all_members = await find_all_users_in_group_chat(db, telegram_group_chat_id)
-    message = create_message_to_mention_members(all_members)
+    group_chat_members = await find_all_users_in_group_chat(db, telegram_group_chat_id)
+
+    if not group_chat_members:
+        logger.info(f"Attempted to shout all members in telegram chat id '{telegram_group_chat_id}' but there are no members.")
+        return "I don't know any members in this chat. If you want me to register someone ask them to send a message."
+
+    message = create_message_to_mention_members(group_chat_members)
     return message
 
 
@@ -59,9 +71,16 @@ async def shout_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> N
     args = context.args
     session = get_database()
 
+    telegram_chat_id = update.effective_chat.id
+
     if len(args) == 1:
-        message = await shout_subgroup_members(session, update.effective_chat.id, args[0])
+        subgroup_name = args[0]
+        message = await shout_subgroup_members(session, telegram_chat_id, subgroup_name)
+
         await update.message.reply_text(message, parse_mode='markdown')
+        return
+
     else:
-        message = await shout_all_members(session, update.effective_chat.id)
+        message = await shout_all_members(session, telegram_chat_id)
         await update.message.reply_text(message, parse_mode='markdown')
+        return
--- tests/test_shout.py
+++ tests/test_shout.py
@@ -40,6 +40,19 @@ async def test_shout_all_members_if_members_have_special_chars_in_username(db: S
 
 
 @pytest.mark.asyncio
+async def test_shout_all_members_handles_no_registered_members(db: Session):
+    # Given: A group chat exists, but there are no members in it
+    telegram_group_chat_id = -123456789
+    group_chat = create_test_group_chat(db, telegram_group_chat_id, "Group Chat", [])
+
+    # When: We shout all group members
+    message = await shout_all_members(db, group_chat.telegram_group_chat_id)
+
+    # Then: It mentions them
+    assert message == "I don't know any members in this chat. If you want me to register someone ask them to send a message."
+
+
+@pytest.mark.asyncio
 async def test_shout_subgroup_members(db: Session):
     # Given: A group chat already exists
     john = create_test_user(db, telegram_user_id=12345, username="johndoe", first_name="John", last_name="Doe")
@@ -81,6 +94,26 @@ async def test_shout_subgroup_members_only_mentions_members_for_group_chat(db: S
 
     # Then: It mentions them
     assert message == "@johndoe "
+
+
+@pytest.mark.asyncio
+async def test_shout_subgroup_members_handles_no_subgroup_members(db: Session):
+    # Given: A group chat already exists
+    john = create_test_user(db, telegram_user_id=12345, username="johndoe", first_name="John", last_name="Doe")
+    jane = create_test_user(db, telegram_user_id=67890, username="janedoe", first_name="Jane", last_name="Doe")
+
+    telegram_group_chat_id = -123456789
+    group_chat = create_test_group_chat(db, telegram_group_chat_id, "Group Chat", [john, jane])
+
+    # And: the group chat has a subgroup without any members
+    subgroup_name = "Archery"
+    create_test_subgroup(db, group_chat.group_chat_id, subgroup_name, [])
+
+    # When: We shout all subgroup members
+    message = await shout_subgroup_members(db, group_chat.telegram_group_chat_id, subgroup_name)
+
+    # Then: It mentions them
+    assert message == "'Archery' subgroup has no members, use /group to add members."
 
 
 @pytest.mark.asyncio
